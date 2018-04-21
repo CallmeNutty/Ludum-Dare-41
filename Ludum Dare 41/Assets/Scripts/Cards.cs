e67485a62cdb59b6c6840817aed358900b4d1a07
TheNuttyGamer <33847045+TheNuttyGamer@users.noreply.github.com>
2018-04-21 09:08:29 +0100

    Began working on card framework

---
+++ Ludum Dare 41/Assets/Scripts/Cards.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class Cards : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject projectile;
+
+    void OnMouseUp()
+    {
+        ThrowBall(projectile);
+        print("CLICKED CARD");
+    }
+
+    private void ThrowBall(GameObject projectile)
+    {
+        //Spawn projectile to the right side of the camera
+        GameObject spawnedObject = Instantiate(projectile, Camera.main.ViewportToWorldPoint(new Vector3(1.1f, 0.6f)), Quaternion.identity) as GameObject;
+        //Make Object Visible
+        spawnedObject.transform.position = new Vector3(spawnedObject.transform.position.x, spawnedObject.transform.position.y, 0);
+    }
+}
---
+++ Ludum Dare 41/Assets/Scripts/ExtensionMethods.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public static class ExtensionMethods
+{
+    //Method which looks at an object
+    public static void LookAt2D(this Transform transform, Vector2 target)
+    {
+        Vector2 current = transform.position;
+        var direction = target - current;
+        var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+    public static void Shuffle<T>(this IList<T> list)
+    {
+        System.Random rng = new System.Random();
+        int n = list.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = rng.Next(n + 1);
+            T value = list[k];
+            list[k] = list[n];
+            list[n] = value;
+        }
+    }
+
+    public static Color ChangeAlpha(this Color color, float alpha)
+    {
+        return new Color(color.r, color.g, color.b, alpha);
+    }
+}
